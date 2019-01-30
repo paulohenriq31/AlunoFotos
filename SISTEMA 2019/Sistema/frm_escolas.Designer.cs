83a2b8b82383d805896ae058d916cce45660c8dc
Paulo <ph744915@gmail.com>
2019-01-30 10:44:26 -0200

    Projeto 2019

---
+++ SISTEMA 2019/Sistema/frm_escolas.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Sistema
+{
+    partial class frm_cad_escolas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_cad_escolas));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textnmescola = new System.Windows.Forms.TextBox();
+            this.textnmdir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textnmcor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textqtalunos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_novo = new System.Windows.Forms.Button();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome da escola:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textnmescola
+            // 
+            this.textnmescola.Location = new System.Drawing.Point(147, 6);
+            this.textnmescola.Name = "textnmescola";
+            this.textnmescola.Size = new System.Drawing.Size(100, 20);
+            this.textnmescola.TabIndex = 1;
+            this.textnmescola.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textnmdir
+            // 
+            this.textnmdir.Location = new System.Drawing.Point(147, 32);
+            this.textnmdir.Name = "textnmdir";
+            this.textnmdir.Size = new System.Drawing.Size(100, 20);
+            this.textnmdir.TabIndex = 3;
+            this.textnmdir.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome da Diretora:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textnmcor
+            // 
+            this.textnmcor.Location = new System.Drawing.Point(147, 58);
+            this.textnmcor.Name = "textnmcor";
+            this.textnmcor.Size = new System.Drawing.Size(100, 20);
+            this.textnmcor.TabIndex = 5;
+            this.textnmcor.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nome da Coordenadora:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textqtalunos
+            // 
+            this.textqtalunos.Location = new System.Drawing.Point(147, 84);
+            this.textqtalunos.Name = "textqtalunos";
+            this.textqtalunos.Size = new System.Drawing.Size(100, 20);
+            this.textqtalunos.TabIndex = 7;
+            this.textqtalunos.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quantidade de alunos:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(828, 408);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // btn_novo
+            // 
+            this.btn_novo.Location = new System.Drawing.Point(269, 23);
+            this.btn_novo.Name = "btn_novo";
+            this.btn_novo.Size = new System.Drawing.Size(75, 23);
+            this.btn_novo.TabIndex = 10;
+            this.btn_novo.Text = "Novo";
+            this.btn_novo.UseVisualStyleBackColor = true;
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(366, 23);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cadastrar.TabIndex = 11;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(462, 23);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(75, 23);
+            this.btn_excluir.TabIndex = 12;
+            this.btn_excluir.Text = "Excluir";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(561, 23);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancelar.TabIndex = 13;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // frm_cad_escolas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(847, 530);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Controls.Add(this.btn_novo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textqtalunos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textnmcor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textnmdir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textnmescola);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_cad_escolas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro Escola";
+            this.Load += new System.EventHandler(this.frm_cad_escolas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textnmescola;
+        private System.Windows.Forms.TextBox textnmdir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textnmcor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textqtalunos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_novo;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_cancelar;
+    }
+}
---
+++ SISTEMA 2019/Sistema/frm_menu.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Sistema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_escola = new System.Windows.Forms.Button();
+            this.btn_cad_escola = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_escola
+            // 
+            this.btn_escola.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_escola.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_escola.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_escola.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_escola.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_escola.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_escola.Image = global::Sistema.Properties.Resources.novo_png;
+            this.btn_escola.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_escola.Location = new System.Drawing.Point(196, 55);
+            this.btn_escola.Name = "btn_escola";
+            this.btn_escola.Size = new System.Drawing.Size(326, 286);
+            this.btn_escola.TabIndex = 1;
+            this.btn_escola.Text = "ESCOLHER ESCOLAS";
+            this.btn_escola.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_escola.UseVisualStyleBackColor = false;
+            this.btn_escola.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btn_cad_escola
+            // 
+            this.btn_cad_escola.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_cad_escola.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cad_escola.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cad_escola.Location = new System.Drawing.Point(68, 289);
+            this.btn_cad_escola.Name = "btn_cad_escola";
+            this.btn_cad_escola.Size = new System.Drawing.Size(90, 52);
+            this.btn_cad_escola.TabIndex = 2;
+            this.btn_cad_escola.Text = "CADASTRAR ESCOLA";
+            this.btn_cad_escola.UseVisualStyleBackColor = false;
+            this.btn_cad_escola.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(635, 409);
+            this.Controls.Add(this.btn_cad_escola);
+            this.Controls.Add(this.btn_escola);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_escola;
+        private System.Windows.Forms.Button btn_cad_escola;
+    }
+}
+
